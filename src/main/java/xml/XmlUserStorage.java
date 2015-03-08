27d3a01786ec6d2289748c81798d46cbef1560df
ravidfish <ma.fisher7@gmail.com>
2015-03-07 17:35:22 -0700

    Merge origin/master
    Conflicts:
    	src/main/java/xml/XmlUserStorage.java

--- src/main/java/xml/XmlUserStorage.java
+++ src/main/java/xml/XmlUserStorage.java
@@ -30,7 +30,7 @@ public class XmlUserStorage implements XmlStorage {
     private List<DiscussionUsers> userList;
     
     public XmlUserStorage() {
-        file = "..\\src\\main\\resources\\user.xml";
+        file = "resources\\user.xml";
         userList = new ArrayList<DiscussionUsers>();
         read();
     }
