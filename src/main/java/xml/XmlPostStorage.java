966bf4d53179dd69202369dad713f0eafd107c08
ravidfish <ma.fisher7@gmail.com>
2015-03-07 17:25:28 -0700

    changing the path for resources

--- src/main/java/xml/XmlPostStorage.java
+++ src/main/java/xml/XmlPostStorage.java
@@ -30,7 +30,7 @@ public class XmlPostStorage implements XmlStorage {
     private LinkedList<DiscussionPosts> postList;
     
     public XmlPostStorage() {
-        file = "c:\\webtemp\\data.xml";
+        file = "..\\src\\main\\webtemp\\data.xml";
         postList = new LinkedList<DiscussionPosts>();
         read();
     }
